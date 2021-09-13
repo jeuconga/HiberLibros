8c0650f8ce076af0e33381baacda6abbf776fb47
Mohamad <Mohamad@LAPTOP-MOHA.mshome.net>
2021-09-13 12:51:48 +0200

    Commit diario

--- HiberLibros/src/main/java/com/hiberlibros/HiberLibros/HiberLibrosApplication.java
+++ HiberLibros/src/main/java/com/hiberlibros/HiberLibros/HiberLibrosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HiberLibrosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HiberLibrosApplication.class, args);
+	public static void main(String[] args) { 
+		SpringApplication.run(HiberLibrosApplication.class, args); 
 	}
 
 }
