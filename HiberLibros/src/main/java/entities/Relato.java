c9e7097ea68216d05a05950c2ce673964cd4067e
Usuario <Usuario@DESKTOP-2KP4SM8>
2021-09-14 12:52:25 +0200

    relato

---
+++ HiberLibros/src/main/java/entities/Relato.java
@@ -0,0 +1,50 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Relato {
+
+    
+@Id    
+@GeneratedValue(strategy = GenerationType.IDENTITY) 
+private Integer id;
+
+/*
+@ManyToOne             
+@JoinColumn(name = "id_usuario")
+private Usuario usuario;*/
+
+@Column
+private String fichero;
+
+@Column
+private String titulo;
+
+//@ManyToOne             
+//@JoinColumn(name = "id_genero")
+//private Genero genero;
+
+@Column
+private Double valoracionUsuarios;
+
+@Column
+private Integer numeroValoraciones;
+
+
+
+    
+}
