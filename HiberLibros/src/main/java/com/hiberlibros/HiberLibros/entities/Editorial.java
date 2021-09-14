fbda8e5a1d63db86464967670aab8fd5d4105958
Usuario <Usuario@DESKTOP-2KP4SM8>
2021-09-14 13:04:25 +0200

    relato

--- HiberLibros/src/main/java/com/hiberlibros/HiberLibros/entities/Editorial.java
+++ HiberLibros/src/main/java/com/hiberlibros/HiberLibros/entities/Editorial.java
@@ -8,10 +8,11 @@ package com.hiberlibros.HiberLibros.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+
 
 
 /**
