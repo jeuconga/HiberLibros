c6ea9c3636cf895658749dfa7157a1be3d23c460
Isabel <Isabel@192.168.255.35>
2021-09-14 12:41:58 +0200

    Push

--- HiberLibros/src/main/java/com/hiberlibros/HiberLibros/entities/Genero.java
+++ HiberLibros/src/main/java/com/hiberlibros/HiberLibros/entities/Genero.java
@@ -1,10 +1,12 @@
 package com.hiberlibros.HiberLibros.entities;
 
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,5 +27,12 @@ public class Genero {
     private Integer id;
     @Column
     private String nombre;
+    
+   //@OneToMany(mappedBy = "genero")
+   // private List<Relato> listaRelatos;
+    
+   //@OneToMany(mappedBy = "libro")
+   // private List<Libro> listaLibros;
+    
     
 }
