7ad3a70b65f72eeb15d2a0f4cbed243da09a58f1
Usuario <Usuario@DESKTOP-35SEOO3>
2021-09-15 19:00:57 +0200

    Inicio, login, panel principal usuario

--- HiberLibros/src/main/resources/basedatos/driversBD.sql
+++ HiberLibros/src/main/resources/basedatos/driversBD.sql
@@ -16,3 +16,7 @@ create table editoriales (
 insert into editoriales values (1,"alfaguara");
 insert into editoriales values (2,"anaya");
 insert into editoriales values (3,"barco de vapor");
+
+insert into generos values (1, "terror");
+insert into libros values(1, "espanyol", "isbn111", "Crimen y castigo", "",4,1,3);
+insert into usuarios (apellido, ciudad, direccion, mail, nombre, telef) values ("Perez", "Zgz", "Decilias", "zgz@hiberus.com", "Juan", "666666666");
