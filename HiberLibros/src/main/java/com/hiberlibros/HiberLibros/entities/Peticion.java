a939b6ea25d55943b0cb88fb0246957459531f7e
Usuario <Usuario@DESKTOP-5K4L85V>
2021-09-15 10:45:42 +0200

    Merge origin/master
    Conflicts:
    	HiberLibros/src/main/java/com/hiberlibros/HiberLibros/services/EditorialService.java

---
+++ HiberLibros/src/main/java/com/hiberlibros/HiberLibros/entities/Peticion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hiberlibros.HiberLibros.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="peticiones")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Peticion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+}
