0ad20aebc5af2fcb904ab317cb30de8f78a92f7a
Usuario <Usuario@DESKTOP-35SEOO3>
2021-09-15 09:53:54 +0200

    Cambios 14/09 tarde

--- HiberLibros/src/main/java/com/hiberlibros/HiberLibros/controllers/UsuarioController.java
+++ HiberLibros/src/main/java/com/hiberlibros/HiberLibros/controllers/UsuarioController.java
@@ -6,9 +6,11 @@
 package com.hiberlibros.HiberLibros.controllers;
 
 import com.hiberlibros.HiberLibros.entities.Usuarios;
-import com.hiberlibros.HiberLibros.repositories.UsuariosRepository;
+import com.hiberlibros.HiberLibros.interfaces.UsuariosServiceI;
+import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +20,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UsuarioController {
     
     @Autowired
-    private UsuariosRepository ruService;
+    private UsuariosServiceI service;
     
     @GetMapping
-    public String usuarioFormulario(){
+    public String usuarioFormulario(Model m, String registro){        
+        m.addAttribute("registro", registro);
+        m.addAttribute("usuarios", service.usuariosList());
         return "/usuarios/usuariosFormulario";
     }
     
     @PostMapping("/guardarUsuario")
     public String usuarioRegistrar(Usuarios u){
-        
-        ruService.save(u);
+        String resultado=service.guardarUsuario(u);
+        return "redirect:/usuarios?registro="+resultado;
+    }
+    
+    @GetMapping("/borrar")
+    public String borrar(Integer id){
+        service.borrarUsuario(id);
         return "redirect:/usuarios";
     }
     
