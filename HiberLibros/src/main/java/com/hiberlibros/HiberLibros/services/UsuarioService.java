6f052d1005610d8244bb1c755ffa03d3b345542e
Usuario <Usuario@DESKTOP-35SEOO3>
2021-09-20 13:26:33 +0200

    Merge origin/master
    Conflicts:
    	HiberLibros/src/main/java/com/hiberlibros/HiberLibros/controllers/UsuarioController.java
    	HiberLibros/src/main/java/com/hiberlibros/HiberLibros/interfaces/UsuarioServiceI.java
    	HiberLibros/src/main/java/com/hiberlibros/HiberLibros/services/UsuarioService.java

--- HiberLibros/src/main/java/com/hiberlibros/HiberLibros/services/UsuarioService.java
+++ HiberLibros/src/main/java/com/hiberlibros/HiberLibros/services/UsuarioService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.hiberlibros.HiberLibros.repositories.UsuarioRepository;
 import com.hiberlibros.HiberLibros.interfaces.IUsuarioService;
-import com.hiberlibros.HiberLibros.interfaces.UsuarioServiceI;
 import java.util.Optional;
 import javax.transaction.Transactional;
 
